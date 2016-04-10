f46b62b50a07317ff6d6ce71877077d7020aa470
Elmar Burke <hallo@elmarburke.de>
2016-04-10 23:47:01 +0200

    map features

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var map = new L.map('map')
+var map = new L.map('map', {
+  maxZoom: 12
+})
 // 5.8777,51.0828,7.8113,51.9307
 map.fitBounds([[51.0828,5.8777],[51.9307,7.8113]])
 
@@ -31,13 +33,12 @@ Promise.all([verspaetungPromise, linesPromises])
     var minVerspaetung = Math.min.apply(null, verspaetungenValues)
     var maxVerspaetung = Math.max.apply(null, verspaetungenValues)
 
-    var scale = chroma.scale(['lightgray', 'yellow', 'orange', 'red']).domain([minVerspaetung, maxVerspaetung], 7, 'k-means')
+    var scale = chroma.scale(['lightgray', 'orange', 'red']).domain([minVerspaetung, maxVerspaetung])
 
     for (var i in lines.objects) {
       var linename = i.toUpperCase()
       if (validLines.indexOf(linename) !== -1) {
         var line = topojson.feature(lines, lines.objects[i])
-        console.log('looking for', linename)
         addLine(linename, line, 2015)
       }
     }
